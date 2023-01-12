4a165f4a4c4f081d56f7506ee5b9b8d93f73ef1f
blur <thetobiterra@gmail.com>
2023-01-12 04:47:26 +0100

    minor

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,6 +4,7 @@ export default function ExpenseItem(props){
     const month = props.date.toLocaleString('en-US', {month: 'long'})
     const day = props.date.toLocaleString('en-US', {day: '2-digit'})
     const year = props.date.getFullYear()
+    
 
     return(
         <div className="expense-item">
